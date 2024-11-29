a122415288c6b90830490afefb3a48fbbcb482e4
motion5 <robert.fallaize@insurxglobal.com>
2024-11-29 17:06:27 +0000

    Add float_opts to floating terminal mapping

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,7 +1,7 @@
 -- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
----@type ChadrcConfig
+---@class CustomChadrc
 local M = {}
 
 M.ui = {
@@ -10,6 +10,30 @@ M.ui = {
   tabufline = {
     --  more opts
     order = { "buffers", "tabs", "btns" },
+  },
+}
+
+
+-- Define custom key mappings
+M.mappings = {
+  general = {
+    -- Map Alt+i to toggle the floating terminal in normal and terminal modes
+    ["<A-i>"] = {
+      function()
+        require("nvchad.term").toggle {
+          pos = "float",
+          id = "floatTerm",
+          float_opts = {
+            row = 0.35,
+            col = 0.05,
+            width = 0.9,
+            height = 0.8,
+          },
+        }
+      end,
+      "Toggle floating terminal",
+      mode = { "n", "t" },
+    },
   },
 }
 
