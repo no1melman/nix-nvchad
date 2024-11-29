bf721a1f66f1e3ef725af86caa21252f2045c1c1
motion5 <robert.fallaize@insurxglobal.com>
2024-11-29 17:43:49 +0000

    Use new mapping format

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,7 +1,7 @@
 -- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
----@class CustomChadrc
+---@type ChadrcConfig
 local M = {}
 
 M.ui = {
@@ -10,30 +10,6 @@ M.ui = {
   tabufline = {
     --  more opts
     order = { "buffers", "tabs", "btns" },
-  },
-}
-
-
--- Define custom key mappings
-M.mappings = {
-  general = {
-    -- Map Alt+i to toggle the floating terminal in normal and terminal modes
-    ["<A-i>"] = {
-      function()
-        require("nvchad.term").toggle {
-          pos = "float",
-          id = "floatTerm",
-          float_opts = {
-            row = 0.35,
-            col = 0.05,
-            width = 0.9,
-            height = 0.8,
-          },
-        }
-      end,
-      "Toggle floating terminal",
-      mode = { "n", "t" },
-    },
   },
 }
 
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -41,3 +41,13 @@ end, { desc = "Debug go test" })
 map("n", "<leader>dgl", function()
   require("dap-go").debug_last()
 end, { desc = "Debug last go test" })
+
+
+map({ "n", "t" }, "<A-i>", function()
+    require("nvchad.term").toggle { pos = "float", id = "floatTerm", float_opts={
+        row = 0.15,
+        col = 0.07,
+        width = 0.85,
+        height = 0.7
+    }}
+end, { desc = "terminal toggle floating term" })
